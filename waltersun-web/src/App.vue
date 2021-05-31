28a43c5189361e9403b00d14de8952954dabc81e
swhmonster <swh7941@qq.com>
2021-05-31 20:48:32 +0800

    【+】element-ui引入(饿了么)

--- waltersun-web/src/App.vue
+++ waltersun-web/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <FileOperation></FileOperation>
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+                 background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+          <el-menu-item index="1">Downloads 下载</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <FileOperation></FileOperation>
+      </el-main>
+      <el-footer>Copyright © 2020-2021</el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -11,6 +22,17 @@ export default {
   name: 'App',
   components: {
     FileOperation
+  },
+  data() {
+    return {
+      activeIndex: '1'
+    }
+  },
+  methods: {
+    handleSelect()
+    {
+
+    }
   }
 }
 </script>
