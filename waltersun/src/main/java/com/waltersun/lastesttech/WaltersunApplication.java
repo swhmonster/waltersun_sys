ea5b91b241af7f9aeadefbf7dc87ba737c40e132
swhmonster <swh7941@qq.com>
2021-06-01 20:55:28 +0800

    【*】mybatis集成

--- waltersun/src/main/java/com/waltersun/lastesttech/WaltersunApplication.java
+++ waltersun/src/main/java/com/waltersun/lastesttech/WaltersunApplication.java
@@ -1,8 +1,10 @@
 package com.waltersun.lastesttech;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan({"com.waltersun.lastesttech.mapper"})
 @SpringBootApplication
 public class WaltersunApplication {
 
