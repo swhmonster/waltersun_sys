0678178dbe2390e86e134315b3f02180a84ca467
swhmonster <swh7941@qq.com>
2021-06-03 15:55:20 +0800

    【*】配置文件修改

--- waltersun/src/main/java/com/waltersun/lastesttech/service/impl/CommonServiceImpl.java
+++ waltersun/src/main/java/com/waltersun/lastesttech/service/impl/CommonServiceImpl.java
@@ -1,6 +1,7 @@
 package com.waltersun.lastesttech.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.waltersun.lastesttech.mapper.CommonMapper;
 import com.waltersun.lastesttech.service.CommonService;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
  * @author walter
  * @date 2021-06-03 11:31
  */
+@Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class CommonServiceImpl implements CommonService {
     private final CommonMapper commonMapper;
