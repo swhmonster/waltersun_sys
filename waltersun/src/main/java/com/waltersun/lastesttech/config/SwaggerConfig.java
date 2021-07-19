f17a52e9aa2c1915e051dc09e28f86c4e24bf38d
swhmonster <swh7941@qq.com>
2021-07-19 20:55:49 +0800

    【*】swagger配置调整

--- waltersun/src/main/java/com/waltersun/lastesttech/config/SwaggerConfig.java
+++ waltersun/src/main/java/com/waltersun/lastesttech/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.waltersun.lastesttech.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +21,13 @@ public class SwaggerConfig {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("Walter Sun's WebSite")
+                        .description("swagger-bootstrap-ui-demo RESTful APIs")
+                        .termsOfServiceUrl("https://www.waltersun.cn")
+                        .contact(new Contact("Walter","https://www.waltersun.cn","swh7941@qq.com"))
+                        .version("1.0")
+                        .build())
                 .groupName("基础功能")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.waltersun.lastesttech.controller"))
