9090796a7ba251d969d2f4834c9a52efc15546d4
swhmonster <swh7941@qq.com>
2021-09-01 20:08:46 +0800

    【*】sonar修改

--- waltersun/src/main/java/com/waltersun/lastesttech/aspect/MyAspect.java
+++ waltersun/src/main/java/com/waltersun/lastesttech/aspect/MyAspect.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSON;
